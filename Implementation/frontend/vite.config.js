71e2bf47cd6d2efc86d3a76fb177c2a4a9ac3046
Charlie Medialdea <charlz1202@yahoo.com>
2025-03-18 22:38:08 -0700

    Fix: Fix API routing issue in production

--- Implementation/frontend/vite.config.js
+++ Implementation/frontend/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 const isProduction = process.env.NODE_ENV === 'production';
 
 const API_URL = isProduction
-  ? "https://furbot-production.up.railway.app"  // Production backend
+  ? "https://furbot-production.up.railway.app/api"  // Production backend
   : "http://127.0.0.1:5000";  // Local backend
 
 export default defineConfig({
